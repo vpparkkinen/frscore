d3869d358f4ff1da69896bf53b7dd45de0bb207a
vpfof <parkkinenv@gmail.com>
2020-10-20 13:21:16 +0200

    fix 'output' arg

--- frscore_functions.R
+++ frscore_functions.R
@@ -215,12 +215,12 @@ frscored_cna <- function(x,
 ## output = "asf" returns asfs, output = "csf" csfs.
 
 rean_cna <- function(..., what = "c", 
-                   attempt = seq(1, 0.7, -0.05), ncsf = 20, output = "csf"){
+                   attempt = seq(1, 0.7, -0.05), ncsf = 20, output = c("csf", "asf")){
   
   cl <- match.call()
   dots <- list(...)
   if (any(c("cov", "con", "con.msc") %in% names(dots))){
-    stop("cna arguments 'con', 'cov', 'con.msc' not applicable")
+    stop("cna arguments 'con', 'cov', 'con.msc' not meaningful")
   }
   cl$attempt <- cl$asf <- cl$ncsf <- cl$csf <- cl$output <- NULL
   cl[[1]] <- as.name("cna")
@@ -231,8 +231,8 @@ rean_cna <- function(..., what = "c",
   for (i in 1:length(sols)){
       cl$con <- ccargs[i,"lowfirst"]
       cl$cov <- ccargs[i, "lowsec"]
-    if (output=="csf"){sols[[i]] <- csf(eval(cl), n = ncsf)} 
-    if (output=="asf"){sols[[i]] <- asf(eval(cl))}
+    if (output[[1]]=="csf"){sols[[i]] <- csf(eval(cl), n = ncsf)} 
+    if (output[[1]]=="asf"){sols[[i]] <- asf(eval(cl))}
     dt <- data.frame(cnacon = rep(cl$con, nrow(sols[[i]])), 
                                   cnacov = rep(cl$cov, nrow(sols[[i]])))
     sols[[i]] <- cbind(sols[[i]], dt)
