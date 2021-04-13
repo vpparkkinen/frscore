956924c6e9f8b867de621750aa1c3bf3f694e4e1
VPlt1 <parkkinenv@gmail.com>
2021-04-13 14:07:06 +0200

    add m_error data set

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' Simulated data of sixteen cases with measurement error in one case
+#'
+#' A simulated set of crisp-set configurational data
+#' that conforms to the structure
+#' A + B*C <-> E except for one case, 'c16',
+#' which simulates measurement error in that case,
+#' and including one irrelevant factor "D".
+"m_error"
